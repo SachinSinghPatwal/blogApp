4003bba253eff183bd9a9e16435ca785440fda39
sachinsinghpatwal <sachinsingh71004@gmail.com>
2025-01-13 17:40:17 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
     );
   }
   return (
-    <div className="w-full py-8 ">
+    <div className="w-full py-8 min-h-[48.7vh]">
       <Container>
         <div className="flex flex-wrap">
           {posts.map((post) => (
